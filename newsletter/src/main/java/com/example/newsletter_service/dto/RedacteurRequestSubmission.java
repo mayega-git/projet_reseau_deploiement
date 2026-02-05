1e9c8ddcc3b2b48da62cf97eff417bc9e6bb906d
mayega-git <maybalock@gmail.com>
2026-02-05 16:00:00 +0100

    fix:submitRedacteur

--- newsletter/src/main/java/com/example/newsletter_service/dto/RedacteurRequestSubmission.java
+++ newsletter/src/main/java/com/example/newsletter_service/dto/RedacteurRequestSubmission.java
@@ -1,34 +1,33 @@
 package com.example.newsletter_service.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-
 // DTO pour soumettre une demande d'inscription
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RedacteurRequestSubmission {
-    
+
     @NotBlank(message = "L'email est obligatoire")
     @Email(message = "Format d'email invalide")
     private String email;
-    
+
     @NotBlank(message = "Le mot de passe est obligatoire")
     @Size(min = 8, message = "Le mot de passe doit contenir au moins 8 caractères")
     private String password;
-    
+
     @NotBlank(message = "Le nom est obligatoire")
     private String nom;
-    
+
     @NotBlank(message = "Le prénom est obligatoire")
     private String prenom;
 }
-
-
-
-
